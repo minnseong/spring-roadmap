68f963d7655728c8cf281ce8739060ebeb325695
minnseong <kdsvip5@naver.com>
2022-01-07 18:07:37 +0900

    회원 도메인 개발 및 테스트 (순수 java)

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        // memberService - MemberService 인터페이스 의존 + MemberServiceImpl 구현체 의존 - 추상화 구체화 둘다 의존
+        // -> DIP 원칙 위반
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}

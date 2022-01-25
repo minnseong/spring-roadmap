fac30b88b05bc40ad0d37e0e808b14f927584778
minnseong <kdsvip5@naver.com>
2022-01-25 17:17:22 +0900

    Model 추가 + viewResolver - v3

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public ModelView process(Map<String, String> parmaMap) {
+        String username = parmaMap.get("username");
+        int age = Integer.parseInt(parmaMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView mv = new ModelView("save-result");
+        mv.getModel().put("member", member);
+        return mv;
+    }
+}

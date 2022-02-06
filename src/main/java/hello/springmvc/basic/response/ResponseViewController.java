f5af9fd365419d7cd1193e5d8955a693bfe4eb68
minnseong <kdsvip5@naver.com>
2022-02-07 00:46:20 +0900

    Spring HTTP 응답 - 정적 리소스, 뷰 템플릿 (thymeleaf)

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseView1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseView2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    // 권장하지 않는 방법
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+}

883930c610749a6dde64b0c5e9d193515ac76dc3
minnseong <kdsvip5@naver.com>
2022-03-07 16:24:51 +0900

    자바스크립트 인라인, 템플릿 레이아웃

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -113,6 +113,14 @@ public class BasicController  {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model)
+    {
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
